849eafc6cdfe16d54ce09cbe1cccbba135f81be7
FradiMohamedAmine <aminfradi614@gmail.com>
2024-07-18 12:38:20 +0100

    undo old way likes/dislikes and creation of m and c Reaction as polymorphic association  to handle them

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,12 +10,5 @@ class Comment < ApplicationRecord
   after_create_commit -> { broadcast_prepend_later_to "comments" }
   after_update_commit -> { broadcast_replace_later_to "comments", target: "comment_#{self.id}" }
 
-  after_initialize :set_defaults, unless: :persisted?
 
-  private
-
-  def set_defaults
-    self.likes ||= 0
-    self.dislikes ||= 0
-  end
 end
